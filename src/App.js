95b0cd3ab46fb225d33d120882a42cb9646aa7a9
maugares <mauro.garciaesteban@gmail.com>
2019-04-06 22:50:13 +0200

    add component to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PizzaListContainer from './components/PizzaListContainer'
 
 class App extends Component {
   render() {
